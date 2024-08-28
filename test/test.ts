b6a814387fbcc92591e2ec32f7c803f8714a22aa
ash24r <ashish@odenlabs.co>
2024-08-28 16:39:11 +0400

    update readme and test script

--- test/test.ts
+++ test/test.ts
@@ -7,13 +7,15 @@ const configService = new ConfigService();
 async function testPolymarket() {
   const polymarketConnector = new PolymarketConnector(configService);
   await polymarketConnector.init();
-  await polymarketConnector.createLimitOrder("21742633143463906290569050155826241533067272736897614950488156847949938836455", 0.1, 5, Side.BUY);
+  await polymarketConnector.createLimitOrder("21742633143463906290569050155826241533067272736897614950488156847949938836455", 0.01, 5, Side.BUY);
+  await polymarketConnector.fetchUSDCBalance();
 }
 
 async function testDrift() {
   const driftConnector = new DriftConnector(configService);
   await driftConnector.init();
-  await driftConnector.createLimitOrder("TRUMP-WIN-2024-BET", 0.1, 4, Side.BUY);
+  await driftConnector.createLimitOrder("TRUMP-WIN-2024-BET", 0.01, 4, Side.BUY);
+  console.log(await driftConnector.fetchUSDCBalance());
 }
 
 testPolymarket();
